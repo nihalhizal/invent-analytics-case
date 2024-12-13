2f2fdccee1294d5f710ed69a16fe6fd114b5d7b2
Nihal Hızal <78216352+nihalhizal@users.noreply.github.com>
2024-12-13 22:26:22 +0300

    create movie detail page

---
+++ app/movie-detail/[imdbID]/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "next/navigation";
+import { getMovieDetailAsync } from "@/store/slices/movieSlice";
+import { Paper, Box, Typography, Grid } from "@mui/material";
+
+const MovieDetail = () => {
+  const dispatch = useDispatch();
+  const { imdbID } = useParams();
+  const { movieDetail, loading, error } = useSelector(
+    (state: any) => state.movies
+  );
+
+  useEffect(() => {
+    if (imdbID) {
+      dispatch(getMovieDetailAsync(imdbID));
+    }
+  }, [dispatch, imdbID]);
+
+  if (loading)
+    return <Typography className="detail-page">Loading...</Typography>;
+  if (error)
+    return <Typography className="detail-page">Error: {error}</Typography>;
+  if (!movieDetail)
+    return <Typography className="detail-page">No movie found!</Typography>;
+
+  const details = [
+    { label: "Year", value: movieDetail.Year },
+    { label: "Genre", value: movieDetail.Genre },
+    { label: "Plot", value: movieDetail.Plot },
+    { label: "Director", value: movieDetail.Director },
+    { label: "Actors", value: movieDetail.Actors },
+    { label: "Language", value: movieDetail.Language },
+    { label: "Released", value: movieDetail.Released },
+    { label: "Runtime", value: movieDetail.Runtime },
+    {
+      label: "IMDb Rating",
+      value: `${movieDetail.imdbRating} (${movieDetail.imdbVotes} votes)`,
+    },
+    { label: "Awards", value: movieDetail.Awards },
+    { label: "BoxOffice", value: movieDetail.BoxOffice },
+  ];
+
+  return (
+    <Box className="detail-page bg-gray-100">
+      <Paper className="flex shadow-2xl rounded-2xl w-3/4 max-w-5xl p-6 gap-6">
+        <div className="w-1/3">
+          <img
+            src={movieDetail.Poster}
+            alt={movieDetail.Title}
+            className="w-full h-auto rounded-2xl"
+          />
+        </div>
+
+        <div className="w-2/3">
+          <Typography variant="h4" fontWeight="bold" gutterBottom>
+            {movieDetail.Title}
+          </Typography>
+
+          {details.map((detail, index) => (
+            <Typography key={index} gutterBottom>
+              <strong>{detail.label}:</strong> {detail.value}
+            </Typography>
+          ))}
+        </div>
+      </Paper>
+    </Box>
+  );
+};
+
+export default MovieDetail;
